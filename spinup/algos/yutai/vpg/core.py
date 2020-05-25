81acd6f6d3f52076258f6c579e91dbd55d1164b3
Yutai Zhou <yutai@mit.edu>
2020-05-25 10:00:42 -0400

    boring helpers

--- spinup/algos/yutai/vpg/core.py
+++ spinup/algos/yutai/vpg/core.py
@@ -1,8 +1,13 @@
 import numpy as np
 import scipy.signal
 from gym.spaces import Box, Discrete
-
 import torch
 import torch.nn as nn
 from torch.distributions.normal import Normal
 from torch.distributions.categorical import Categorical
+from typing import Tuple, List
+
+def combined_shape(length: int, shape = None) -> Tuple:
+    if shape is None: 
+        return (length,)
+    return (length, shape) if np.isscalar(shape) else (length, *shape)
