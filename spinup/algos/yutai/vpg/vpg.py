deae5c769c30502a2229ac3c5aa50175c226abbe
Yutai Zhou <yutai@mit.edu>
2020-05-25 12:55:32 -0400

    setting up vpg class

--- spinup/algos/yutai/vpg/vpg.py
+++ spinup/algos/yutai/vpg/vpg.py
@@ -80,5 +80,68 @@ class Buffer:
                 logprob: self.logprob_buffer}
         return {k: torch.as_tensor(v, dtype=torch.float32) for k, v in data.items()}
 
+class VPG:
+    """
+    VPG w/ GAE-Lambda
+    """
+    def __init__(self,
+                 env_maker: Callable,
+                 ac_maker = core.MLPActorCritic,
+                 ac_kwargs = {},
+                 seed: int = 0,
+                 epochs: int = 50,
+                 steps_per_epoch: int = 4000,
+                 gamma: float = 0.99,
+                 actor_lr: float = 3e-4,
+                 critic_lr: float = 1e-3,
+                 num_iter_train_critic: int = 80,
+                 lam: float = 0.97,
+                 max_episode_len: int = 1000,
+                 logger_kwargs={},
+                 save_freq: int = 10)
+        # Special function to avoid certain slowdowns from PyTorch + MPI combo.
+        setup_pytorch_for_mpi()
+        # Set up logger and save configuration
+        self.logger = EpochLogger(**logger_kwargs)
+        self.logger.save_config(locals())
+        # Random seed
+        seed += 10000 * proc_id()
+        torch.manual_seed(seed)
+        np.random.seed(seed)
 
+        # make env
+        self.env = env_maker()
+        self.obs_dim = self.env.observation_space.shape
+        self.act_dim = self.env.action_space.shape
+
+        # make actor-critic
+        self.ac = ac_maker(self.env.observation_space, self.env.action_space, **ac_kwargs)
+
+        # make buffer
+        self.local_steps_per_epoch = int(steps_per_epoch/num_procs())
+        self.buffer = Buffer(self.obs_dim, self.act_dim, self.local_steps_per_epoch, gamma, lam)
+        
+        # make optimizers
+        self.actor_optimizer = Adam(ac.actor.parameters(), lr=actor_lr)
+        self.critic_optimizer = Adam(ac.critic.parameters(), lr=critic_lr)
+
+
+        # Sync params across processes
+        sync_params(ac)
+        # Count variables
+        var_counts = tuple(core.count_vars(module) for module in [self.ac.pi, self.ac.v])
+        logger.log('\nNumber of parameters: \t pi: %d, \t v: %d\n'%var_counts)
+        # Set up model saving
+        logger.setup_pytorch_saver(ac)
+
+    def compute_actor_loss(self, data):
+        obs, act, adv, logprob_old = data['obs'], data['act'], data['adv'], data['logp']
+
+        # policy loss
+        
+        # extra info
+
+    def compute_critic_loss(self, data):
+        obs, ret = data['obs'], data['ret']
+        return ((self.ac.critic(obs) - ret)**2).mean()
 
