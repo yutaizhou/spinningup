28207e7c9380a5d53d166e45becaeb89d96181d2
Yutai Zhou <yutai@mit.edu>
2020-05-26 22:16:03 -0400

    vpg shenanigans

--- spinup/algos/yutai/vpg/core.py
+++ spinup/algos/yutai/vpg/core.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 from torch.distributions.normal import Normal
 from torch.distributions.categorical import Categorical
-from typing import Sequence, Tuple, List, Callable
+from typing import Sequence, Tuple, List, Callable, Optional
 
 def combined_shape(length: int, shape = None) -> Tuple:
     if shape is None: 
@@ -25,7 +25,7 @@ def mlp(sizes: List[int],
     for i in range(len(sizes) - 1): # the in-betweens
         activation_fn = hidden_activation if i < len(sizes) - 2 else output_activation
         layers.extend([nn.Linear(sizes[i], sizes[i+1]), activation_fn()])
-        return nn.Sequential(*layers)
+    return nn.Sequential(*layers)
 
 
 class Actor(nn.Module):
--- spinup/algos/yutai/vpg/vpg.py
+++ spinup/algos/yutai/vpg/vpg.py
@@ -3,7 +3,8 @@ import torch
 from torch.optim import Adam
 import gym
 import time
-import spinup.algos.pytorch.vpg.core as core
+from typing import Callable, Optional
+import spinup.algos.yutai.vpg.core as core
 from spinup.utils.logx import EpochLogger
 from spinup.utils.mpi_pytorch import setup_pytorch_for_mpi, sync_params, mpi_avg_grads
 from spinup.utils.mpi_tools import mpi_fork, mpi_avg, proc_id, mpi_statistics_scalar, num_procs
@@ -54,7 +55,7 @@ class Buffer:
 
 		# GAE-Lambda
 		deltas = rewards[:-1] + self.gamma * values[1:] - values[:-1]
-		self.adv_buf[episode_slice] = core.discount_cumsum(deltas, self.gamma * self.lam)
+		self.advantage_buffer[episode_slice] = core.discount_cumsum(deltas, self.gamma * self.lam)
 
 		# r2g
 		self.return_buffer[episode_slice] = core.discount_cumsum(rewards, self.gamma)[:-1]
@@ -109,6 +110,12 @@ class VPG:
 		torch.manual_seed(seed)
 		np.random.seed(seed)
 
+		self.epochs = epochs
+		self.steps_per_epoch = steps_per_epoch
+		self.num_iter_train_critic = num_iter_train_critic
+		self.max_episode_len = max_episode_len
+		self.save_freq = save_freq
+
 		# make env
 		self.env = env_maker()
 		self.obs_dim = self.env.observation_space.shape
@@ -127,21 +134,145 @@ class VPG:
 
 
 		# Sync params across processes
-		sync_params(ac)
+		sync_params(self.ac)
 		# Count variables
-		var_counts = tuple(core.count_vars(module) for module in [self.ac.pi, self.ac.v])
-		logger.log('\nNumber of parameters: \t pi: %d, \t v: %d\n'%var_counts)
+		var_counts = tuple(core.count_vars(module) for module in [self.ac.actor, self.ac.critic])
+		self.logger.log('\nNumber of parameters: \t pi: %d, \t v: %d\n'%var_counts)
 		# Set up model saving
-		logger.setup_pytorch_saver(ac)
+		self.logger.setup_pytorch_saver(self.ac)
 
 	def compute_actor_loss(self, data):
-		obs, act, adv, logprob_old = data['obs'], data['act'], data['adv'], data['logp']
+		obs, act, adv, logprob_old = data['obs'], data['act'], data['adv'], data['logprob']
 
 		# policy loss
+		pi, logprob = self.ac.actor(obs, act)
+		loss_actor = -(logprob_old * adv).mean()
 		
 		# extra info
+		approx_kl = (logprob_old - logprob).mean().item()
+		entropy = pi.entropy().mean().item()
+		pi_info = dict(kl=approx_kl, entropy=entropy)
+
+		return loss_actor, pi_info
 
 	def compute_critic_loss(self, data):
 		obs, ret = data['obs'], data['ret']
 		return ((self.ac.critic(obs) - ret)**2).mean()
 
+	def update(self):
+		data = self.buffer.get()
+
+		actor_loss_old, actor_info_old = self.compute_actor_loss(data)
+		actor_loss_old = actor_loss_old.item()
+		critic_loss_old = self.compute_critic_loss(data).item()
+
+		# train policy
+		self.actor_optimizer.zero_grad()
+		actor_loss, actor_info = self.compute_actor_loss(data)
+		actor_loss.backward()
+		mpi_avg_grads(self.ac.actor)
+		self.actor_optimizer.step()
+
+		# train critic
+		for i in range(self.num_iter_train_critic):
+			self.critic_optimizer.zero_grad()
+			critic_loss = self.compute_critic_loss(data)
+			critic_loss.backward()
+			mpi_avg_grads(self.ac.critic)
+			self.critic_optimizer.step()
+
+		#log 
+		kl, entropy = actor_info['kl'], actor_info['ent']
+		self.logger.store(LossPi=actor_loss_old, LossV=critic_loss_old,
+						  KL=kl, Entropy=entropy,
+						  DeltaLossV=(critic_loss.item() - critic_loss_old),
+						  DeltaLossPi=(actor_loss.item() - actor_loss_old))
+
+						  
+
+	def train(self):
+		start_time = time.time()
+		obs, episode_ret, episode_len = self.env.reset(), 0, 0
+
+		for epoch in range(self.epochs):
+			for t in range(self.local_steps_per_epoch):
+				act, v, logprob = self.ac.step(torch.as_tensor(obs, dtype=torch.float32))
+				# print(f"act: {act}")
+				# print(f"v: {v}")
+				# print(f"logprob: {logprob}")
+				
+				obs_next, reward, done, _ = self.env.step(act)
+				episode_ret += reward
+				episode_len += 1
+
+				self.buffer.store(obs, act, reward, v, logprob)
+				self.logger.store(VVals=v)
+
+				obs = obs_next
+
+				# episode end/timeout logic
+				timeout = (episode_len == self.max_episode_len)
+				terminal = (done or timeout)
+				epoch_ended = (t == self.local_steps_per_epoch - 1)
+
+				if terminal or epoch_ended:
+					if epoch_ended and not terminal:
+						print(f"Warning: trajectory cut off by epoch at {episode_len} steps")
+					if timeout or epoch_ended:
+						_, v, _ = self.ac.step(torch.as_tensor(obs, dtype=torch.float32))
+					else:
+						v = 0
+					self.buffer.finish_path(v)
+					if terminal:
+						self.logger.store(EpRet=episode_ret, EpLen=episode_len)
+					obs, episode_ret, episode_len = self.env.reset(), 0, 0
+
+			if (epoch % self.save_freq == 0) or (epoch == self.epochs - 1):
+				self.logger.save_state({"env": self.env}, None)
+
+			self.update()
+
+			# Log info about epoch
+			self.logger.log_tabular('Epoch', epoch)
+			self.logger.log_tabular('EpRet', with_min_and_max=True)
+			self.logger.log_tabular('EpLen', average_only=True)
+			self.logger.log_tabular('VVals', with_min_and_max=True)
+			self.logger.log_tabular('TotalEnvInteracts', (epoch+1)*steps_per_epoch)
+			self.logger.log_tabular('LossPi', average_only=True)
+			self.logger.log_tabular('LossV', average_only=True)
+			self.logger.log_tabular('DeltaLossPi', average_only=True)
+			self.logger.log_tabular('DeltaLossV', average_only=True)
+			self.logger.log_tabular('Entropy', average_only=True)
+			self.logger.log_tabular('KL', average_only=True)
+			self.logger.log_tabular('Time', time.time()-start_time)
+			self.logger.dump_tabular()
+
+if __name__ == '__main__':
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--env', type=str, default='CartPole-v0')
+	parser.add_argument('--hid', type=int, default=64)
+	parser.add_argument('--l', type=int, default=2)
+	parser.add_argument('--gamma', type=float, default=0.99)
+	parser.add_argument('--seed', '-s', type=int, default=0)
+	parser.add_argument('--cpu', type=int, default=4)
+	parser.add_argument('--steps', type=int, default=4000)
+	parser.add_argument('--epochs', type=int, default=50)
+	parser.add_argument('--exp_name', type=str, default='vpg')
+	args = parser.parse_args()
+
+	mpi_fork(args.cpu)  # run parallel code with mpi
+
+	from spinup.utils.run_utils import setup_logger_kwargs
+	logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
+
+	vpg = VPG(lambda : gym.make(args.env),
+			  ac_maker=core.MLPActorCritic,
+			  ac_kwargs=dict(hidden_sizes=[args.hid]*args.l),
+			  gamma=args.gamma,
+			  seed=args.seed,
+			  steps_per_epoch=args.steps,
+			  epochs=args.epochs,
+			  logger_kwargs=logger_kwargs)
+
+	vpg.train()
